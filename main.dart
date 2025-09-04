68935d1313a85c70f2e753d09095a9fa1c888fa7
Bielfxz <gf499673@gmail.com>
2025-09-04 00:00:04 +0000

    feat: is statement

--- main.dart
+++ main.dart
@@ -1,3 +1,23 @@
+import 'dart:io';
+
 void main(){
-    print("Ola mundo!");
+
+    print("Informe seu nome: ");
+    String? nome = stdin.readLineSync();
+
+    int idade =();
+        if(idade >= 18){
+            print('Pode entrar na balada');
+        }else{ //if (idade <= 17){
+           print('Fim do programa');
+    }
 }
+
+
+/*print("Informe a sua idade:");
+    int idade = 18;
+        if(idade >= 18){
+            print('Pode entrar na balada');
+        }else{ 
+           print('Fim do programa');
+        }*/
