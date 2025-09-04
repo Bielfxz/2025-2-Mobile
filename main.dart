ca7433be85ee7c84542334bc1eafdfbe720a285c
Bielfxz <gf499673@gmail.com>
2025-09-04 00:51:10 +0000

    feat: comando-else-if

--- main.dart
+++ main.dart
@@ -2,16 +2,48 @@ import 'dart:io';
 
 void main(){
 
-    print("Informe seu nome: ");
+print("Informe a sua renda: ");
+    String? input = stdin.readLineSync();
+    int renda = int.parse(input!);
+    
+    int saldo;
+
+    if(renda >= 1400 && renda < 4999){
+        print("varejo");
+    }else if(renda >=5000){
+        print("vangogh");
+    }else if()
+
+} 
+/*String? input = stdin.readLineSync();
+int renda = int.parse(input!);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* int idade= idade;
+
+    print("Informe sua idade: ");
     String? nome = stdin.readLineSync();
 
-    int idade =();
         if(idade >= 18){
             print('Pode entrar na balada');
-        }else{ //if (idade <= 17){
+        }else{ /*if (idade <= 17){*/
            print('Fim do programa');
     }
-}
+}*/
 
 
 /*print("Informe a sua idade:");
